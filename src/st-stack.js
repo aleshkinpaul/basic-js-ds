11221d886d8fd1c1b2ad47fd9617d3491e800cb7
Pavel Aleshkin <vinci.aleshkin@gmail.com>
2021-10-18 16:10:11 +0300

    feat: add st-stack

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,18 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.stack = [];
+  }
+
+  push(element) {
+    this.stack.push(element);
+    return this.stack;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let lastItem = this.stack[this.stack.length - 1] || undefined;
+    this.stack.pop();
+    return lastItem;
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length - 1];
   }
 }
